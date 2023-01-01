2a01ef419b1af8edc0c0eee6a9595e31856d9eb6
GeffDev <geffgeffdev@gmail.com>
2023-01-01 10:51:03 +0800

    me after conforming to the zig naming conventions
    "what did andrew kelley do to me?"

--- src/v1.zig
+++ src/v1.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 
 const Directories = struct {
-    dirOffset: u64,
-    dirName: []const u8,
+    dir_offset: u64,
+    dir_name: []const u8,
 };
 
-var vFileSize: u64 = 0;
-var virtualFileOffset: u64 = 0;
+var v_file_size: u64 = 0;
+var virtual_file_offset: u64 = 0;
 
-pub var fileHandle: std.fs.File = undefined;
+pub var file_handle: std.fs.File = undefined;
 pub var cwd: std.fs.Dir = undefined;
 
 pub fn unpackV1DataFile() !void {
@@ -20,105 +20,105 @@ pub fn unpackV1DataFile() !void {
         std.log.err("could not open data file!", .{});
         return;
     };
-    fileHandle = file;
+    file_handle = file;
 
     cwd = std.fs.cwd();
 
-    var fileBuf: u8 = 0;
+    var file_buf: u8 = 0;
     // dirOffset? idk lol
-    var dirThing: u32 = 0;
-    var dirAmount: u8 = 0;
-    var fileHeader: usize = 0;
+    var dir_thing: u32 = 0;
+    var dir_amount: u8 = 0;
+    var file_header: usize = 0;
 
-    try fileHandle.seekTo(0);
+    try file_handle.seekTo(0);
 
-    try fileRead(&fileBuf, 1);
-    dirThing = fileBuf;
-    try fileRead(&fileBuf, 1);
-    dirThing += @as(u32, fileBuf) << 8;
-    try fileRead(&fileBuf, 1);
-    dirThing += @as(u32, fileBuf) << 16;
-    try fileRead(&fileBuf, 1);
-    dirThing += @as(u32, fileBuf) << 24;
-    try fileRead(&dirAmount, 1);
+    try fileRead(&file_buf, 1);
+    dir_thing = file_buf;
+    try fileRead(&file_buf, 1);
+    dir_thing += @as(u32, file_buf) << 8;
+    try fileRead(&file_buf, 1);
+    dir_thing += @as(u32, file_buf) << 16;
+    try fileRead(&file_buf, 1);
+    dir_thing += @as(u32, file_buf) << 24;
+    try fileRead(&dir_amount, 1);
 
     // array length must be comptime known so lets just
     // set this to 64 i guess
     var directories: [64]Directories = std.mem.zeroes([64]Directories);
 
     var i: usize = 0;
-    while (i < dirAmount) : (i += 1) {
-        var dirRead: [64]u8 = std.mem.zeroes([64]u8);
-        var dirNameLen: u8 = 0;
-        try fileRead(&dirNameLen, 1);
+    while (i < dir_amount) : (i += 1) {
+        var dir_read: [64]u8 = std.mem.zeroes([64]u8);
+        var dir_name_len: u8 = 0;
+        try fileRead(&dir_name_len, 1);
 
-        var dirIndex: u8 = 0;
-        while (dirIndex < dirNameLen) : (dirIndex += 1) {
-            try fileRead(&dirRead[dirIndex], 1);
+        var dir_index: u8 = 0;
+        while (dir_index < dir_name_len) : (dir_index += 1) {
+            try fileRead(&dir_read[dir_index], 1);
         }
 
-        const slicedDir = try allocator.dupe(u8, dirRead[0..dirNameLen]);
-        directories[i].dirName = slicedDir;
+        const sliced_dir = try allocator.dupe(u8, dir_read[0..dir_name_len]);
+        directories[i].dir_name = sliced_dir;
 
-        try fileRead(&fileBuf, 1);
-        fileHeader = fileBuf;
-        try fileRead(&fileBuf, 1);
-        fileHeader += @as(u32, fileBuf) << 8;
-        try fileRead(&fileBuf, 1);
-        fileHeader += @as(u32, fileBuf) << 16;
-        try fileRead(&fileBuf, 1);
-        fileHeader += @as(u32, fileBuf) << 24;
+        try fileRead(&file_buf, 1);
+        file_header = file_buf;
+        try fileRead(&file_buf, 1);
+        file_header += @as(u32, file_buf) << 8;
+        try fileRead(&file_buf, 1);
+        file_header += @as(u32, file_buf) << 16;
+        try fileRead(&file_buf, 1);
+        file_header += @as(u32, file_buf) << 24;
 
-        virtualFileOffset = fileHeader + dirThing;
-        directories[i].dirOffset = virtualFileOffset;
+        virtual_file_offset = file_header + dir_thing;
+        directories[i].dir_offset = virtual_file_offset;
     }
 
     i = 0;
-    var curDir: std.fs.Dir = undefined;
-    while (i < dirAmount) : (i += 1) {
-        curDir = try cwd.makeOpenPath(directories[i].dirName, .{});
-        try file.seekTo(directories[i].dirOffset);
+    var cur_dir: std.fs.Dir = undefined;
+    while (i < dir_amount) : (i += 1) {
+        cur_dir = try cwd.makeOpenPath(directories[i].dir_name, .{});
+        try file.seekTo(directories[i].dir_offset);
 
-        while (try file.getPos() != directories[i + 1].dirOffset) {
-            var fName: [64]u8 = std.mem.zeroes([64]u8);
-            var fNameLen: u8 = 0;
-            fileRead(&fNameLen, 1) catch |err| {
+        while (try file.getPos() != directories[i + 1].dir_offset) {
+            var f_name: [64]u8 = std.mem.zeroes([64]u8);
+            var f_name_len: u8 = 0;
+            fileRead(&f_name_len, 1) catch |err| {
                 if (err == error.EndOfStream) {
                     std.log.info("unpacked all files", .{});
                 }
                 return;
             };
 
-            var fLenIndex: usize = 0;
-            while (fLenIndex < fNameLen) : (fLenIndex += 1) {
-                try fileRead(&fName[fLenIndex], 1);
+            var f_len_index: usize = 0;
+            while (f_len_index < f_name_len) : (f_len_index += 1) {
+                try fileRead(&f_name[f_len_index], 1);
             }
 
             // getting away with not using an allocator :smirk:
-            std.log.info("unpacking file: {s}{s}", .{ directories[i].dirName, fName });
+            std.log.info("unpacking file: {s}{s}", .{ directories[i].dir_name, f_name });
 
-            const slicedFName = try allocator.dupe(u8, fName[0..fNameLen]);
+            const sliced_f_name = try allocator.dupe(u8, f_name[0..f_name_len]);
 
-            try fileRead(&fileBuf, 1);
-            vFileSize = fileBuf;
-            try fileRead(&fileBuf, 1);
-            vFileSize += @as(u32, fileBuf) << 8;
-            try fileRead(&fileBuf, 1);
-            vFileSize += @as(u32, fileBuf) << 16;
-            try fileRead(&fileBuf, 1);
-            vFileSize += @as(u32, fileBuf) << 24;
+            try fileRead(&file_buf, 1);
+            v_file_size = file_buf;
+            try fileRead(&file_buf, 1);
+            v_file_size += @as(u32, file_buf) << 8;
+            try fileRead(&file_buf, 1);
+            v_file_size += @as(u32, file_buf) << 16;
+            try fileRead(&file_buf, 1);
+            v_file_size += @as(u32, file_buf) << 24;
 
-            const unpackedFile = try curDir.createFile(slicedFName, .{});
-            try writeFile(unpackedFile);
-            allocator.free(slicedFName);
+            const unpacked_file = try cur_dir.createFile(sliced_f_name, .{});
+            try writeFile(unpacked_file);
+            allocator.free(sliced_f_name);
         }
     }
 }
 
-fn fileRead(dest: *u8, bytesToRead: usize) !void {
+fn fileRead(dest: *u8, bytes_to_read: usize) !void {
     var i: usize = 0;
-    while (i < bytesToRead) : (i += 1) {
-        dest.* = try fileHandle.reader().readByte();
+    while (i < bytes_to_read) : (i += 1) {
+        dest.* = try file_handle.reader().readByte();
     }
 }
 
@@ -126,7 +126,7 @@ fn fileRead(dest: *u8, bytesToRead: usize) !void {
 /// please speed this up
 fn writeFile(file: std.fs.File) !void {
     var i: usize = 0;
-    while (i < vFileSize) : (i += 1) {
-        try file.writer().writeByte(try fileHandle.reader().readByte());
+    while (i < v_file_size) : (i += 1) {
+        try file.writer().writeByte(try file_handle.reader().readByte());
     }
 }
